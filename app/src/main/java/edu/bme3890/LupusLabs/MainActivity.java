140bf723abbb01c2e8a052d3d764c30ef09a3fb5
fixbc <barrett.c.fix@vanderbilt.edu>
2020-03-17 19:25:22 -0700

    3/17 7:25

---
+++ app/src/main/java/edu/bme3890/LupusLabs/MainActivity.java
@@ -0,0 +1,37 @@
+package edu.bme3890.LupusLabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import edu.bme3890.LupusLabs.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openMainMenuActivity(View v) {
+        Intent intent = new Intent(this, MainMenuActivity.class);
+        startActivity(intent);
+    }
+
+    public void openCreateAccountActivity(View v) {
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
+    }
+
+    public void openForgotPasswordActivity(View v) {
+        Intent intent = new Intent(this, ForgotPasswordActivity.class);
+        startActivity(intent);
+    }
+}
