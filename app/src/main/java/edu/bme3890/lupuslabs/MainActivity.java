a905fcca354404c9b91a15f35a5e0bcf689f9539
vastolagit <56359312+vastolagit@users.noreply.github.com>
2020-04-12 15:47:30 -0500

    create account button functionality. adds email and password to firestone database

--- app/src/main/java/edu/bme3890/lupuslabs/MainActivity.java
+++ app/src/main/java/edu/bme3890/lupuslabs/MainActivity.java
@@ -5,13 +5,66 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import edu.bme3890.lupuslabs.Model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+    //Firebase
+    FirebaseDatabase database;
+    DatabaseReference users;
+
+    EditText emailEditText, passwordEditText;
+    Button createAccButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        //Firebase
+        database = FirebaseDatabase.getInstance();
+        users = database.getReference("Users");
+
+        emailEditText = (EditText) findViewById(R.id.emailEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        createAccButton = (Button) findViewById(R.id.createAccButton);
+
+        createAccButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final User user = new User(emailEditText.getText().toString(),
+                        passwordEditText.getText().toString());
+
+                users.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.child(user.getEmail()).exists())
+                            Toast.makeText(MainActivity.this, "This email is already registered", Toast.LENGTH_SHORT).show();
+                        else {
+                            users.child(user.getEmail()).setValue(user);
+                            Toast.makeText(MainActivity.this, "Sign up Successful", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        //
+
+                    }
+                });
+            }
+        });
     }
 
     public void openMainMenuActivity(View v) {
---
+++ app/src/main/java/edu/bme3890/lupuslabs/Model/User.java
@@ -0,0 +1,33 @@
+package edu.bme3890.lupuslabs.Model;
+
+public class User {
+
+    private String password;
+    private String email;
+
+    public User() {
+
+    }
+
+    public User(String email, String password) {
+
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
